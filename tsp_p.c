e5373b7a6c9b70ce02311ad0c715e239b1b8b5c1
Kristel Tan <ktan@bu.edu>
2015-11-11 22:08:45 -0500

    Added reading in the input graph in main( )

--- tsp_p.c
+++ tsp_p.c
@@ -3,13 +3,13 @@
 // Due: November 13, 2015
 
 // tsp_p.c
-// Multi-process version of the Travelling Salesman Problem
 // Reads in an input graph and returns all possible paths from a given
 // starting node and their respective distances
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_LINE 15
+#define MAX_ROW 12
 
 // Calculates all paths given a starting node
 void allPaths(int start, int des, int x, int m[x][x], int visited[], int path[], int currentIndex) {
@@ -40,10 +40,61 @@ void allPaths(int start, int des, int x, int m[x][x], int visited[], int path[],
 
 int main(void) {
     
-    int m[2][2] = {0, 1, 1, 0};
-    int visited[] = {0, 0};
-    int path[MAX_LINE];
+    int i, j, k;        // Integers declared for loops
+    int numCities;      // Will store the total number of cities
+    int m[10][10];      // Max size of input graph is 10
     
-    allPaths(1, 1, 2, m, visited, path, 0);
+    // Retrieve the number of desired cities using standard input
+    printf("Enter the total number of cities (i.e. nodes): \n");
+    if ( (scanf("%d", &numCities) == 1) && (numCities <= 10) && (numCities > 1)) {
+        printf("OK, numCities = %d \n", numCities);
+    }
+    else {
+        // Exit if numCities is not a valid int or between and including 2 and 10
+        printf("tsp_p: Number of cities must be a valid integer and within the bounds of 2 and 10\n\n");
+        exit(0);
+    }
+    
+    // Retrieve the input graph, which will contain distances between each node
+    printf("\nNow let's construct the input graph... \n");
+    for (i = 0; i < numCities; i++) {
+        printf("Enter the elements of row # %d: \n", i + 1);
+        for (j = 0; j < numCities; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+    
+    // Check that the diagonal of the input graph has a distance of 0
+    k = 0;
+    while (k < numCities) {
+        if (m[k][k] != 0) {
+            printf("\ntsp_p: The diagonal of the input graph must have distance of 0 \n\n");
+            exit(0);
+        }
+        else {
+            k++;
+        }
+    }
+    
+    // Check that the distances in the input graph are less than the max distance of 255
+    for (i = 0; i < numCities; i++) {
+        for (j = 0; j < numCities; j++) {
+            if (m[i][j] > 255) {
+                printf("\ntsp_p: The maximum distance between two cities is 255... ");
+                printf("invalid distance found at m[%d][%d] (= %d). \n\n", i, j, m[i][j]);
+                exit(0);
+            }
+        }
+    }
+    
+    // Confirm the input graph is valid by printing it
+    printf("\nOK, the input graph is: \n\n");
+    for (i = 0; i < numCities; i++) {
+        for (j = 0; j < numCities; j++) {
+            printf("\t%d", m[i][j]);
+        }
+        printf("\n");
+    }
+    
     
 }
