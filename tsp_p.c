38aeeabc571ab0b022f1ba9ca808b65a01199463
Kristel Tan <ktan@bu.edu>
2015-11-13 00:43:09 -0500

    Changes to tsp_p.c

--- tsp_p.c
+++ tsp_p.c
@@ -9,31 +9,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define MAX_ROW 12
+#define SHM_SIZE 1024
 
 /* Returns array of adjacent nodes to given city */
 int * adjacent(int start, int size, int m[size][size]) {
     
     int *adjNodes = malloc(sizeof(int) * (size - 1));       // Allocate memory for adjNodes
     int count = 0;                                          // Will keep track of index for adjNodes
+    int i = 0;
     
     // Searches down a column in the input graph for valid distances
     // and stores the node in adjNodes if valid
-    for (int i = 0; i < size; i++) {
-        if (m[i][start] > 0) {
-            adjNodes[count] = i;
+    
+    for (i = 0; i < size; i++) {
+        if ((m[start][i] != -1) && (start != i)) {
+            adjNodes[count] =  i;
             count++;
         }
     }
+    /*
+    int *ptr = adjNodes;
+    printf("\nadjNodes = { ");
+    for (int j = 0; j < count; j++) {
+        printf("%d,", ptr[j]);
+    }
+    printf("}\n");*/
     return adjNodes;
 }
 
 /* Calculates total distance of a given traveled path */
-int calcDistance(int path[], int currentIndex, int x, int m[x][x]) {
+int calcDistance(int path[], int currentIndex, int size, int m[size][size]) {
     int sumDist = m[path[0]][path[1]];
+    int i;
     if (currentIndex > 2) {
-        for (int i = 2; i < currentIndex; i++) {
+        for (i = 2; i < currentIndex; i++) {
             sumDist = sumDist + m[path[i - 1]][path[i]];
         }
     }
@@ -41,27 +54,36 @@ int calcDistance(int path[], int currentIndex, int x, int m[x][x]) {
 }
 
 /* Calculates all valid paths given a starting node and its adjacent nodes */
-void allPaths(int start, int des, int x, int m[x][x], int visited[], int path[], int currentIndex) {
+void allPaths(int start, int des, int size, int m[size][size], int visited[], int path[], int currentIndex) {
     
     visited[start] = 1;
     path[currentIndex] = start;
     currentIndex++;
+    int i;
     
     if (start == des) {
-        for (int i = 0; i < currentIndex; i++) {
-            printf("%d ", path[i]);
+        for (i = 0; i < currentIndex; i++) {
+            fprintf(stdout, "%d ", path[i]);
         }
-        printf("%d", path[0]);
-        int sumDist = calcDistance(path, currentIndex, x, m);
-        printf("\t\t total distance: %d", sumDist);
-        printf("\n");
+        fprintf(stdout, "%d", path[0]);
+        int sumDist = calcDistance(path, currentIndex, size, m);
+        fprintf(stdout, "\t --> total distance: %d", sumDist);
+        fprintf(stdout, "\n");
     }
     else {
-        int *adjNodes = adjacent(start, x, m);
-        int *ptr = adjNodes;
-        for (int i = 0; i < (sizeof(ptr) / sizeof(int)); i++) {
+        int adjLength = 0;
+        int *adjNodes = adjacent(start, size, m);
+        
+        for (i = 0; i < size; i++) {
+            if ((m[start][i] != -1) && (start != i)) {
+                adjLength++;
+            }
+        }
+
+        for (i = 0; i < adjLength; i++) {
             if (visited[adjNodes[i]] != 1) {
-                allPaths(adjNodes[i], des, x, m, visited, path, currentIndex);
+                allPaths(adjNodes[i], des, size, m, visited, path, currentIndex);
+                continue;
             }
         }
     }
@@ -76,24 +98,24 @@ int main(void) {
     int numCities;      // Will store the total number of cities
     
     // Retrieve the number of desired cities using standard input
-    printf("Enter the total number of cities (i.e. nodes): \n");
-    if ( (scanf("%d", &numCities) == 1) && (numCities <= 10) && (numCities > 1)) {
-        printf("OK, numCities = %d \n", numCities);
+    fprintf(stdout, "Enter the total number of cities (i.e. nodes): \n");
+    if ((fscanf(stdin, "%d", &numCities) == 1) && (numCities <= 10) && (numCities > 1)) {
+        fprintf(stdout, "OK, numCities = %d \n", numCities);
     }
     else {
         // Exit if numCities is not a valid int or between and including 2 and 10
-        printf("tsp_p: Number of cities must be a valid integer and within the bounds of 2 and 10\n\n");
+        fprintf(stderr, "tsp_p: Number of cities must be a valid integer and within the bounds of 2 and 10\n\n");
         exit(1);
     }
     
     int m[numCities][numCities];      // Max size of input graph is 10
     
     // Retrieve the input graph, which will contain distances between each node
-    printf("\nNow let's construct the input graph... \n");
+    fprintf(stdout, "\nNow let's construct the input graph... \n");
     for (i = 0; i < numCities; i++) {
-        printf("Enter the elements of row # %d: \n", i + 1);
+        fprintf(stdout, "Enter the elements of row # %d: \n", i + 1);
         for (j = 0; j < numCities; j++) {
-            scanf("%d", &m[i][j]);
+            fscanf(stdin,"%d", &m[i][j]);
         }
     }
     
@@ -101,7 +123,7 @@ int main(void) {
     k = 0;
     while (k < numCities) {
         if (m[k][k] != 0) {
-            printf("\ntsp_p: The diagonal of the input graph must have distance of 0 \n\n");
+            fprintf(stderr, "\ntsp_p: The diagonal of the input graph must have distance of 0 \n\n");
             exit(1);
         }
         else {
@@ -113,40 +135,66 @@ int main(void) {
     for (i = 0; i < numCities; i++) {
         for (j = 0; j < numCities; j++) {
             if (m[i][j] > 255) {
-                printf("\ntsp_p: The maximum distance between two cities is 255... ");
-                printf("invalid distance found at m[%d][%d] (= %d). \n\n", i, j, m[i][j]);
+                fprintf(stderr, "\ntsp_p: The maximum distance between two cities is 255... ");
+                fprintf(stderr, "invalid distance found at m[%d][%d] (= %d). \n\n", i, j, m[i][j]);
                 exit(1);
             }
         }
     }
     
     // Confirm the input graph is valid by printing it
-    printf("\nOK, the input graph is: \n\n");
+    fprintf(stdout, "\nOK, the input graph is: \n\n");
     for (i = 0; i < numCities; i++) {
         for (j = 0; j < numCities; j++) {
-            printf("\t%d", m[i][j]);
+            fprintf(stdout, "\t%d", m[i][j]);
         }
-        printf("\n");
+        fprintf(stdout, "\n");
     }
     
-    printf("\n");
-    printf("All possible paths for this input graph: \n");
+    fprintf(stdout, "\n");
+    fprintf(stdout, "All possible paths for this input graph: \n");
     
-    int status = 0;
-    for (int city = 0; city < numCities; city++) {
-        pid_t pid = fork();
+    //int status = 0;
+    int city, des;
+    //int shmid;
+    //char *shm;
+    
+    // For each city, fork a new child process and calculate all the valid paths to the other cities
+    
+    for (city = 0; city < numCities; city++) {
+        for (des = 0; des < numCities; des++) {
+            int path[MAX_ROW];
+            int visited[numCities];
+            if (des != city) {
+                allPaths(city, des, numCities, m, visited, path, 0);
+            }
+        }
+    }
+    
+    for (city = 0; city < numCities; city++) {
+        
+        int shmid;
+        int *result;
+        int status = 0;
+        
+        shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
+        result = (int*)shmat(shmid, NULL,0);
+        
+        int pid = fork();
         if (pid == -1) {
             fprintf(stderr, "tsp_p: fork failed\n");
+            shmdt(result);
             exit(1);
         }
         else if (pid == 0) {
-            for (int des = 0; des < numCities; des++) {
+            for (des = 0; des < numCities; des++) {
                 int path[MAX_ROW];
                 int visited[numCities];
                 if (des != city) {
                     allPaths(city, des, numCities, m, visited, path, 0);
                 }
             }
+            shmdt(result);
             exit(0);
         }
         else {
